1d59a413fd31cbc9f899f3cee33be5b1ee5fb174
Avijit Dey <110467299+6AvijitDey9@users.noreply.github.com>
2022-11-22 04:40:45 -0800

    Update Fruits_Details.sql

--- Fruits_Details.sql
+++ Fruits_Details.sql
@@ -1,13 +1,13 @@
 use avijit_dey;
 create table Fruits (Fname varchar(20) not null, Fnum bigint(11), 
-Flocation varchar(30) not null unique, Fspecification varchar(10) not null);
-alter table Fruits Add Fid int after Fname;
-alter table Fruits modify Fspecification varchar(20) not null;
-alter table Fruits rename to Fruits_Details;
-insert into Fruits_Details values('Mango', 12, 123456, 'Kolkata', 'Sweet');
+Flocation varchar(30) not null unique, Fspecification varchar(10) not null); #Creating table named Fruits
+alter table Fruits Add Fid int after Fname; #Add new column
+alter table Fruits modify Fspecification varchar(20) not null; #Modifying a specific column
+alter table Fruits rename to Fruits_Details; #Rename the table
+insert into Fruits_Details values('Mango', 12, 123456, 'Kolkata', 'Sweet'); #add values 
 insert into Fruits_Details values('Banana', 13, 123457, 'Bengal', 'Sweet');
 insert into Fruits_Details values('Apple', 14, 123458, 'Kashmir', 'Sweet');
 insert into Fruits_Details values('Cherry', 15, 123459, 'Ladakh', 'Sweet');
 insert into Fruits_Details values('Coconut', 16, 123450, 'Tamil', 'Sweet');
-truncate table Fruits_Details;
-Drop table Fruits_Details;
+truncate table Fruits_Details; #Delete rows
+Drop table Fruits_Details; #Delete the table
