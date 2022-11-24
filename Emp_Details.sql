e7882b3dda0421d91fcb0f05393cf50d5afcc157
Avijit Dey <110467299+6AvijitDey9@users.noreply.github.com>
2022-11-24 01:42:03 -0800

    Update Emp_Details.sql

--- Emp_Details.sql
+++ Emp_Details.sql
@@ -1,19 +1,19 @@
 use avijit_dey;
-create table Emp_Details (Id numeric(2), Id_name varchar(10), Basic_salary numeric(6), Designation varchar(10), age numeric(2));
-alter table Emp_Details modify Basic_salary integer(6);
-alter table Emp_Details modify Id_name varchar(15);
-create table Emp_Details_trainee(trainee_Id numeric(2), Id_name varchar(10), Basic_salary numeric(6), Designation varchar(10), age numeric(2));
-insert into Emp_Details values 
+create table Emp_Details (Id numeric(2), Id_name varchar(10), Basic_salary numeric(6), Designation varchar(10), age numeric(2)); #Create table
+alter table Emp_Details modify Basic_salary integer(6); #Modify column type
+alter table Emp_Details modify Id_name varchar(15); #Modify column type
+create table Emp_Details_trainee(trainee_Id numeric(2), Id_name varchar(10), Basic_salary numeric(6), Designation varchar(10), age numeric(2)); #Create table
+insert into Emp_Details values  #Insert values into table
 (1, 'Ruman', 6700, 'Manager', 28),
 (2, 'Rohit', 7000, 'Manager', 24),
 (3, 'Ranjan', 7500, 'Manager', 29),
 (4, 'Ronin', 8000, 'Manager', 30),
 (5, 'Ranjha', 9000, 'Manager', 32);
-insert into Emp_Details_trainee select * from Emp_Details;
+insert into Emp_Details_trainee select * from Emp_Details; 
 select * from Emp_Details, Emp_Details_trainee;
 alter table Emp_Details add skills varchar(15) after age;
 alter table Emp_Details add DOJ date after skills;
-update Emp_Details set skills= 'Java' where Id=1;
+update Emp_Details set skills= 'Java' where Id=1; #Update data
 update Emp_Details set skills= 'C' where Id=2;
 update Emp_Details set skills= 'C++' where Id=3;
 update Emp_Details set skills= 'Python' where Id=4;
@@ -27,12 +27,12 @@ alter table Emp_Details_trainee modify Designation varchar(18);
 update Emp_Details_trainee set Designation = 'Senior IT specialist';
 update Emp_Details set skills = 'HTML' where skills = 'Rust';
 delete from Emp_Details where DOJ='2022-02-21';
-select * from Emp_Details where Basic_salary='9000';
-select * from Emp_Details where Basic_salary='9000' and Id_name='Ranjha';
-select * from Emp_Details where Designation='Manager' or Id_name='Ranjha';
-select * from Emp_Details where Basic_salary in ('9000', '7000');
-select * from Emp_Details where Id_name like 'R%';
-select * from Emp_Details where Id_name like '%N';
-select * from Emp_Details where Id_name like '%N%';
-select * from Emp_Details where Id_name like 'R%N';
-select * from Emp_Details where Id_name like 'Ru_an';
+select * from Emp_Details where Basic_salary='9000'; #Select with condition
+select * from Emp_Details where Basic_salary='9000' and Id_name='Ranjha'; #Select with and condition
+select * from Emp_Details where Designation='Manager' or Id_name='Ranjha'; #Select with or condition
+select * from Emp_Details where Basic_salary in ('9000', '7000'); #Select with in condition
+select * from Emp_Details where Id_name like 'R%'; #Select with like condition, prename
+select * from Emp_Details where Id_name like '%N'; #Select with like condition, postname
+select * from Emp_Details where Id_name like '%N%'; #Select with like condition, middile
+select * from Emp_Details where Id_name like 'R%N'; #Starts with and ends with
+select * from Emp_Details where Id_name like 'Ru_an'; #Blank space in middle of name
