4a7387a935121b16f055478cf08ef42d484ab172
Avijit Dey <110467299+6AvijitDey9@users.noreply.github.com>
2022-12-03 23:39:28 +0530

    Add files via upload

---
+++ Bank.sql
@@ -0,0 +1,2 @@
+use assignments;
+create table Bank(account_no int(4), account_holder varchar(20), ifsc_code varchar(20), branch varchar(10), account_type varchar(10), balance numeric(12,2), pin numeric(4));
